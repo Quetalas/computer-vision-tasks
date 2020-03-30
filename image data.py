e547aaee24f3704a08eba4bc173607c3c40a2513
Evgeny <quetalasj@gmail.com>
2020-03-30 22:45:52 +0300

    add interactive functionality and decide to go to tkinter

--- image data.py
+++ image data.py
@@ -1,31 +1,70 @@
 import numpy as np
 import cv2
-import matplotlib.pyplot as plt
+import tkinter as tk
 
-class Clicker:
-    clickedCoords = []
-    @staticmethod
-    def leftClick(event, x, y, flags, img):
-        if event == cv2.EVENT_LBUTTONDOWN:
-            try:
-                id = Clicker.clickedCoords.index((x, y))
-                pass
-            except ValueError:
-                Clicker.drawRectangle(img, (x, y))
-                Clicker.clickedCoords.append((x, y))
+class Image:
+    def __init__(self, image, name):
+        self.originalImage = image
+        self.copyImage = np.copy(image)
+        self.name = name
+        self.callbacksSet = False
 
-    @staticmethod
-    def drawRectangle(img, xy, dx=5, dy = 5):
-        global image
-        image = cv2.rectangle(img, (xy[0]-dx, xy[1]-dy), (xy[0]+dx, xy[1]+dy), (0, 0, 255))
+    def Show(self):
+        cv2.imshow(self.name, self.copyImage)
+        if not self.callbacksSet:
+
+            cv2.setMouseCallback(self.name, self.MouseCallback, self.copyImage)
+            self.callbacksSet = True
 
 
-image = cv2.imread(r'C:\Users\Evgen\Documents\My\Projects\computer vision\images\1.jpg')
-cv2.namedWindow('image')
-cv2.setMouseCallback('image', Clicker.leftClick, image)
-while True:
-    cv2.imshow('image', image)
-    key = cv2.waitKey(1)
-    if key == ord('c'):
-        break
+
+    def MouseCallback(self, event, x, y, flags, img):
+        print(event)
+        if event == cv2.EVENT_MOUSEMOVE:
+            self.copyImage = np.copy(self.originalImage)
+            self.copyImage = cv2.rectangle(self.copyImage, (x,y), (x+10, y+10), color=(0,0,255))
+            ProgramManager.activeWindowData = {''}
+
+
+
+class ProgramManager:
+    keys = dict.fromkeys(['h', 'q'], False)
+    activeWindowData = {'Type': '', 'data': ''}
+
+    def __init__(self):
+        self.lastKey = "q"
+        self.windowName = 'program'
+        cv2.namedWindow(self.windowName)
+
+    def start(self):
+        self.doKeyboardCallbacks()
+
+    def doKeyboardCallbacks(self):
+        ProgramManager.keys[self.lastKey] = False
+        key = cv2.waitKey(1)
+        key = int(str(key), 0)
+        try:
+            key = chr(key)
+            self.lastKey = key
+            ProgramManager.keys[key] = True
+        except:
+            pass
+
+
+
+
+if __name__ == '__main__':
+
+    image = cv2.imread(r'C:\Users\Evgen\Documents\My\Projects\computer vision\images\1.jpg')
+    image = Image(image, "image 1")
+    image2 = cv2.imread(r'C:\Users\Evgen\Documents\My\Projects\computer vision\images\2.jpg')
+    image2 = Image(image2, "image 2")
+
+    program = ProgramManager()
+    while True:
+        image.Show()
+        image2.Show()
+        program.start()
+        if ProgramManager.keys['q']:
+            break
 
